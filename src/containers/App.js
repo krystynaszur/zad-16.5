506202c0e895417689d4794dfb99da7891eb5c2a
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-08-15 10:49:19 +0200

    Update production files

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-                Oto komponenty aplikacji.
+                App components
                 <Title title="My awesome list of things to do" taskNumber={this.state.data.length} />
             </div>
         );
